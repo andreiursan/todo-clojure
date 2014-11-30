684683aed84c28ebea484b126931a8402157ca21
Andrei Ursan <hello@andreiursan.com>
2014-11-30 01:37:43 +0100

    welcome jdbc and postgresql to project.clj

--- project.clj
+++ project.clj
@@ -5,7 +5,9 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring "1.3.2"]
-                 [compojure "1.2.2"]]
+                 [compojure "1.2.2"]
+                 [org.clojure/java.jdbc "0.3.6"]
+                 [postgresql/postgresql "9.1-901-1.jdbc4"]]
 
   :min-lein-version "2.0.0"
   :uberjar-name "todo-clojure.jar"
